94ad9f72c35d0914b8255dd29f871566f8f54f4b
ALFICA-Sistemas <alfica.sistemas@gmail.com>
2023-10-25 20:39:01 -0400

    Update del 2023-10-25_20:39

--- bash/Publicar.sh
+++ bash/Publicar.sh
@@ -8,8 +8,8 @@ Yo=$(basename ${0})                                         # Tomar el nombre de
 Yo="${Yo%%.*}"                                              # Eliminar la extension del script
 Yo="($(TZ=":America/Caracas" date +'%Y-%m-%d_%H%M') $Yo)"   # Agregar el time tag
 Deb="$DirTmp/$Pos"-Debug.log                                # Log de debug de la última ejecución
-Dat="$DirTmp/$Pos"_$Yo-10pings.dat                          # Log de 10 intentos de conexion
-Scr="$DirTmp/$Pos"_$Yo-Magick.scr                           # Script para ImageMagick
+# Dat="$DirTmp/$Pos"_$Yo-10pings.dat                          # Log de 10 intentos de conexion
+# Scr="$DirTmp/$Pos"_$Yo-Magick.scr                           # Script para ImageMagick
 
 # Sincronizar a Github pages
 cd $(dirname "$0")    # Cambiar al directorio de este script (está eb /bash debajo de la raiz del repo)
