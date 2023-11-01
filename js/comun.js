c7bbf5660691ef601ec7b8f758a9980512dd1d01
ALFICA-Sistemas <alfica.sistemas@gmail.com>
2023-11-01 15:39:01 -0400

    Update del 2023-11-01_15:39

--- js/comun.js
+++ js/comun.js
@@ -16,27 +16,6 @@ function CargaImg(objImagen, URLimagen){
    document.getElementById("fec").innerHTML = F;
 }
 
-function LeerTexto(URL){
-   console.log("LeerTexto en "+URL)
-   var Peticion = new XMLHttpRequest();
-   Peticion.open('GET', URL, true);
-   Peticion.send(null);
-   Peticion.onreadystatechange = function () {
-//   console.log("readystate: "+Peticion.readyState+" Status: "+Peticion.status);
-      if (Peticion.readyState === 4 && Peticion.status === 200) {
-         Leido=Peticion.responseText;
-//         var type = Peticion.getResponseHeader('Content-Type');
-//         console.log("Content-type: "+type+" index="+type.indexOf("text"));
-//         if (type.indexOf("text") !== 1) {
-//            return Peticion.responseText;
-//            }
-//         console.log("Leido:");
-//         console.log(Leido);
-         return Leido;
-      }
-   }
-}
-
 function FechaMod(URL){
    var lastMod = null;
    fetch(URL).then(r => {
---
+++ js/dropdown.js
@@ -0,0 +1,22 @@
+function LeerTexto(URL){
+  console.log("LeerTexto en "+URL)
+  var Peticion = new XMLHttpRequest();
+  Peticion.open('GET', URL, true);
+  Peticion.send(null);
+  Peticion.onreadystatechange = function () {
+//   console.log("readystate: "+Peticion.readyState+" Status: "+Peticion.status);
+     if (Peticion.readyState === 4 && Peticion.status === 200) {
+        Leido=Peticion.responseText;
+//         var type = Peticion.getResponseHeader('Content-Type');
+//         console.log("Content-type: "+type+" index="+type.indexOf("text"));
+//         if (type.indexOf("text") !== 1) {
+//            return Peticion.responseText;
+//            }
+//         console.log("Leido:");
+//         console.log(Leido);
+        return Leido;
+     }
+  }
+}
+
+function LlenarLista()
