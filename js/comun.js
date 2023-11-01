738da47bae1e4d600b3536211af18b237051076f
ALFICA-Sistemas <alfica.sistemas@gmail.com>
2023-11-01 10:39:01 -0400

    Update del 2023-11-01_10:39

--- js/comun.js
+++ js/comun.js
@@ -22,7 +22,7 @@ function LeerTexto(URL){
    Peticion.open('GET', URL, true);
    Peticion.send(null);
    Peticion.onreadystatechange = function () {
-   console.log("readystate: "+Peticion.readyState+" Status: "+Peticion.status);
+//   console.log("readystate: "+Peticion.readyState+" Status: "+Peticion.status);
       if (Peticion.readyState === 4 && Peticion.status === 200) {
          Leido=Peticion.responseText;
 //         var type = Peticion.getResponseHeader('Content-Type');
