937d15c31a976f288fc98c7ddf49a7f38e4d3eae
ALFICA-Sistemas <alfica.sistemas@gmail.com>
2023-11-02 15:49:01 -0400

    Update del 2023-11-02_15:49

--- js/dropdown.js
+++ js/dropdown.js
@@ -3,14 +3,15 @@ function LeerTexto(URL){
   fetch(URL)
   .then((res) => res.text())
   .then((text) => {
-//    console.log(text);
-    return text;
-    // do something with "text"
+    console.log(text);
+    return text;    
    })
   .catch((e) => console.error(e));
 }
 
-function LlenarLista(objLista, Elementos){  
-  objLista.innerHTML += '<option value=Otro></option>';
-  objLista.innerHTML += '<option value="Uno mas"></option>';
+function LlenarLista(objLista, URL){  
+  Lista=LeerTexto(URL);
+  console.log(Lista);
+/*  objLista.innerHTML += '<option value=Otro></option>';
+  objLista.innerHTML += '<option value="Uno mas"></option>'; */
 }
