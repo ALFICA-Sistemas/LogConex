8f111eb6089beeefcd9c0bc2444fd91556d6ac7a
ALFICA-Sistemas <alfica.sistemas@gmail.com>
2023-11-07 14:59:01 -0400

    Update del 2023-11-07_14:59

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,16 +1,29 @@
 function LeerTexto(URL){
+  console.log("LeerTexto");
+  const d = new Date();
+  console.log(d.getMilliseconds());
+
   fetch(URL)
   .then((res) => res.text())
   .then((text) => {
+    console.log(text);
     return text;
    })
   .catch((e) => console.error(e));
 }
 
 function LlenarLista(objLista, URL){  
+  console.log("LlenarLista");
+  const d = new Date();
+  console.log(d.getMilliseconds());
+
   Lista=LeerTexto(URL);             //  Leer el contenido del archivo especificado
 // var Items = Lista.split('\n');    //  Separar a un array cada linea del archivo
   console.log(Lista);
+
+  console.log("LlenarLista");
+  const d = new Date();
+  console.log(d.getMilliseconds());
 //  console.log('');
 //  console.log(Items[0]);
 
