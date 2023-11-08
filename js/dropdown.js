f713cac2388260bad23eff5e6b7188088a800c26
ALFICA-Sistemas <alfica.sistemas@gmail.com>
2023-11-08 15:49:02 -0400

    Update del 2023-11-08_15:49

--- js/dropdown.js
+++ js/dropdown.js
@@ -13,12 +13,12 @@ function LeerTexto(URL) {
 
 function LlenarLista(objLista, URL){
   let Lista = LeerTexto(URL);     //  Leer el contenido del archivo especificado
-// var Items = Lista.split('\n');    //  Separar a un array cada linea del archivo
   console.log(Lista);
-//  console.log('');
-//  console.log(Items[0]);
-
-//  console.log(Lista);
-/*  objLista.innerHTML += '<option value=Otro></option>';
-  objLista.innerHTML += '<option value="Uno mas"></option>'; */
+  console.log('');
+  var Items = Lista.split('\n');    //  Separar a un array cada linea del archivo
+  Items.sort;
+//  Items.reverse;
+  console.log(Items[0]);
+  objLista.innerHTML += '<option value=Otro></option>';
+  objLista.innerHTML += '<option value="Uno mas"></option>';
 }
